1666644469193da8a6e4f57398a865648cd445a6
aryan <Aryan Christian>
2026-01-02 15:02:50 +0530

    small opt

--- sortingTechniques/insertionSort/Main.java
+++ sortingTechniques/insertionSort/Main.java
@@ -9,14 +9,25 @@ public class Main {
     }
 
     public static void insertionSort(int[] arr) {
+        // for (int i = 1; i < arr.length; i++) {
+        //     int j = i;
+        //     while (j > 0 && arr[j] < arr[j - 1]) {
+        //         int temp = arr[j];
+        //         arr[j] = arr[j - 1];
+        //         arr[j - 1] = temp;
+        //         j--;
+        //     }
+        // }
+
+        // optimized approach
         for (int i = 1; i < arr.length; i++) {
-            int j = i;
-            while (j > 0 && arr[j] < arr[j - 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j - 1];
-                arr[j - 1] = temp;
+            int j = i - 1;
+            int key = arr[i];
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
                 j--;
             }
+            arr[j + 1] = key;
         }
     }
 }
